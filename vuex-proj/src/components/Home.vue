8df5beb24a82a25c56c8df02f5e3af4dd28e3da8
wangshiyuan <wangshiyuan@navinfo.com>
2019-08-02 17:42:41 +0800

    add vuex proj

---
+++ vuex-proj/src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <h1>{{$store.state.count}}</h1>
+    <div>{{value}}</div>
+    <show></show>
+    <operation></operation>
+  </div>
+</template>
+<script>
+import show from './Show.vue'
+import operation from './Operation.vue'
+export default {
+  name: 'Home',
+  components: {show, operation},
+  data () {
+    let that = this
+    return {
+      value: that.$store.getters.getterCount
+    }
+  }
+}
+</script>
+<style scoped>
+.container{
+  border: 1px #0094ff solid;
+  border-radius: 3px;
+  height: 100px;
+  margin: 0 200px;
+}
+</style>
---
+++ vuex-proj/src/components/Show.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>{{$store.state.count}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {}
+  }
+}
+</script>
+<style scoped>
+.container{
+  border: 1px #0094ff solid;
+  border-radius: 3px;
+  height: 100px;
+  margin: 5px 200px;
+}
+</style>
